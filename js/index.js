76ab649c241e6f757e216de1ab4e58ae9d28e0d3
Murray WANG <murraywang@hkaift.com>
2025-08-26 14:15:43 +0800

    update data for 20-24

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 Promise.all([
-  d3.csv("data/WORLD_flow_data_new_0123.csv"),
+  d3.csv("data/2020_2024_CounryFlow.csv"),
   d3.json("data/worldmap_topojson_simplified.json"),
   d3.json("data/countries-10m.json"),
   d3.csv("data/country_chinese_name.csv"),
